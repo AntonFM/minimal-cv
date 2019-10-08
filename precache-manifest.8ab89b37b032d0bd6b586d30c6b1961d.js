05d71db6e753d958532f98fe3890a131b12c1a5e
Anton Freyr Magnússon <afm1.h18@nemandi.ntv.is>
2019-10-08 21:14:13 +0000

    Updates

---
+++ precache-manifest.8ab89b37b032d0bd6b586d30c6b1961d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452efb448834f6376598d0585b346f4b",
+    "url": "/AntonFM/minimal-cv.git/index.html"
+  },
+  {
+    "revision": "4cd5a1b73e9c51d86c51",
+    "url": "/AntonFM/minimal-cv.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a0cbb544160e6635a389",
+    "url": "/AntonFM/minimal-cv.git/static/js/2.11012d8f.chunk.js"
+  },
+  {
+    "revision": "4cd5a1b73e9c51d86c51",
+    "url": "/AntonFM/minimal-cv.git/static/js/main.eea22e81.chunk.js"
+  },
+  {
+    "revision": "9df75bc7dc6ede79a471",
+    "url": "/AntonFM/minimal-cv.git/static/js/runtime-main.bc06ceb0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/AntonFM/minimal-cv.git/static/media/logo.25bf045c.svg"
+  }
+]);
