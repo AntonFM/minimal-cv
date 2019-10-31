2703a11035c1fe3fc1cefbf1a5b4148bed7c8e76
Anton Freyr Magnússon <afm1.h18@nemandi.ntv.is>
2019-10-31 21:59:42 +0000

    NOJS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Body from './Components/Body';
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import PropTypes from 'prop-types';
@@ -18,6 +18,12 @@ const StyledArticle = styled.article`
 const StyledDivHeading = styled.div`
   font-size: 4.5em;
   font-weight: 500;
+  width: 50%;
+  border-radius: 10px;
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
 `;
 
 const StyledDivSubHeading = styled.div`
@@ -37,44 +43,55 @@ const StyledPee = styled.p`
 const StyledButton = styled.button``;
 
 const Body = () => {
+  const [accordionClicked, setAccordionClicked] = useState(false);
+  const handler = () => {
+    setAccordionClicked(!accordionClicked);
+  };
+
   return (
     <StyledMain>
       <StyledArticle>
-        <StyledDivHeading>Pied Piper</StyledDivHeading>
+        <StyledDivHeading onClick={handler}>Pied Piper</StyledDivHeading>
+        {accordionClicked && (
+          <React.Fragment>
+            <StyledDivSubHeading>November 2018 - Present</StyledDivSubHeading>
 
-        <StyledDivSubHeading>November 2018 - Present</StyledDivSubHeading>
-        <StyledPee>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at
-          imperdiet ligula. Duis non nibh ultricies, tempor mauris at, finibus
-          dui. Sed sit amet venenatis nisi. Ut volutpat lobortis nulla vitae
-          tincidunt. Ut interdum ullamcorper magna, at aliquet ante luctus non.
-          Maecenas pulvinar vel leo eu lobortis. Nullam eget ligula ipsum.
-        </StyledPee>
+            <StyledPee>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              at imperdiet ligula. Duis non nibh ultricies, tempor mauris at,
+              finibus dui. Sed sit amet venenatis nisi. Ut volutpat lobortis
+              nulla vitae tincidunt. Ut interdum ullamcorper magna, at aliquet
+              ante luctus non. Maecenas pulvinar vel leo eu lobortis. Nullam
+              eget ligula ipsum.
+            </StyledPee>
+          </React.Fragment>
+        )}
       </StyledArticle>
       <StyledArticle>
-        <StyledDivHeading>Aviato</StyledDivHeading>
-        <StyledDivSubHeading>April 2017 - October 2018</StyledDivSubHeading>
-        <StyledPee>
-          Etiam et turpis id eros mollis faucibus eget nec justo. Vivamus
-          interdum leo sapien, sit amet euismod augue porta vitae. Praesent
-          vitae ligula pretium, gravida felis ac, rhoncus orci. Sed et elit eget
-          lectus vulputate venenatis. Quisque gravida lorem quis risus commodo
-          maximus. Aliquam erat volutpat. In a lobortis tellus, eu efficitur
-          libero.
-        </StyledPee>
+        <StyledDivHeading onClick={handler}>Aviato</StyledDivHeading>
+        {accordionClicked && (
+          <>
+            <StyledDivSubHeading>{'title'}</StyledDivSubHeading>
+            <StyledPee>{'text'}</StyledPee>
+          </>
+        )}
       </StyledArticle>
       <StyledArticle>
-        <StyledDivHeading>Hooli</StyledDivHeading>
-        <StyledDivSubHeading>May 2015 - Mars 2017</StyledDivSubHeading>
-        <StyledPee>
-          Quisque ornare sodales metus id dapibus. Quisque sodales aliquet lacus
-          non cursus. In at massa vitae purus congue aliquam. Mauris tempor
-          accumsan fermentum. Aenean volutpat porta libero nec fermentum. Ut
-          finibus nunc id erat venenatis, quis iaculis erat tincidunt. Nullam
-          vitae mauris varius, eleifend risus sed, rhoncus felis. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos.
-        </StyledPee>
+        <StyledDivHeading onClick={handler}>Hooli</StyledDivHeading>
+        {accordionClicked && (
+          <React.Fragment>
+            <StyledDivSubHeading>May 2015 - Mars 2017</StyledDivSubHeading>
+            <StyledPee>
+              Quisque ornare sodales metus id dapibus. Quisque sodales aliquet
+              lacus non cursus. In at massa vitae purus congue aliquam. Mauris
+              tempor accumsan fermentum. Aenean volutpat porta libero nec
+              fermentum. Ut finibus nunc id erat venenatis, quis iaculis erat
+              tincidunt. Nullam vitae mauris varius, eleifend risus sed, rhoncus
+              felis. Class aptent taciti sociosqu ad litora torquent per conubia
+              nostra, per inceptos himenaeos.
+            </StyledPee>
+          </React.Fragment>
+        )}
       </StyledArticle>
     </StyledMain>
   );
